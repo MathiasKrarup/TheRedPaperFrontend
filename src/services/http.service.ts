8a89530cb2a80737f8e9403f5d98340d1c6daec8
MathiasKrarup <math249e@easv365.dk>
2022-11-22 21:22:41 +0100

    Start up

---
+++ src/services/http.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+export const customAxios = axios.create({
+  baseURL: 'https://localhost:5001'
+})
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor() { }
+}
