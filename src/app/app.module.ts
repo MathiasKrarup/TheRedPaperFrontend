b4d1e0a22cfc58ed70c6f1a91518050c212caa0a
MathiasKrarup <math249e@easv365.dk>
2022-11-23 09:13:40 +0100

    login frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const routes: Routes = [{
+  path: 'login', component: LoginComponent
+}, {
+  path: '', redirectTo: 'login', pathMatch: "full"
+}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
