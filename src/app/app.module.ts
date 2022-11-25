ce23b7ff0543ae79d5149c80f75b3f44d610746c
MathiasKrarup <math249e@easv365.dk>
2022-11-25 07:18:13 +0100

    Sidenav
    There's a bug on the loginpage, push for hjælp af alex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import {RouterModule, Routes} from "@angular/router";
+import { MainviewComponent } from './component/mainview/mainview.component';
+import { SidenavComponent } from './component/sidenav/sidenav.component';
+import { CreationsComponent } from './component/creations/creations.component';
+import { BodyComponent } from './component/body/body.component';
 
 
 const routes: Routes = [{
+  path: 'mainview', component: MainviewComponent
+},
+  {
   path: 'login', component: LoginComponent
 }, {
   path: '', redirectTo: 'login', pathMatch: "full"
@@ -17,6 +24,10 @@ const routes: Routes = [{
   declarations: [
     AppComponent,
     LoginComponent,
+    MainviewComponent,
+    SidenavComponent,
+    CreationsComponent,
+    BodyComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
