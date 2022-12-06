c26024d37ee203562e21e6a8513e5bd601a00cd8
JensIssa <71692396+JensIssa@users.noreply.github.com>
2022-12-06 14:09:18 +0100

    Made it!

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../../services/product.service";
+import {Category} from "../../../Interfaces/category";
+import {Subcategory} from "../../../Interfaces/subcategory";
 
 @Component({
   selector: 'app-product-list',
@@ -8,16 +10,20 @@ import {ProductService} from "../../../services/product.service";
 })
 export class ProductListComponent implements OnInit {
 
+  subCategorylist: Subcategory[]
+  categorylist!: Category[]
   productList : any[] = []
   productName : string = "";
   imageUrl : string = "";
   price : number = 0;
+  selectedCategory!: string;
 
   sortby: 'default' | 'htl' | 'lth' | 'atz' | 'zta' = 'default'
   constructor(private http: ProductService) { }
 
   async ngOnInit(){
     this.productList = await this.http.getAllProducts();
+    this.loadCategories()
   }
 
 
@@ -48,5 +54,17 @@ export class ProductListComponent implements OnInit {
   async sortingByZToA(){
     this.productList = await this.http.sortingByZToA();
     this.sortby = 'zta';
+  }
+  private loadCategories(){
+    this.http.getCategoriesObservable().subscribe(data => {
+      this.categorylist = data
+      console.log(data)
+    })
+  }
+  onCategorySelected(selectedCategoryId: any){
+    this.http.getSubCategoriesFromCategory(selectedCategoryId).subscribe(data => {
+      this.subCategorylist = data
+      console.log(data)
+    })
   }
 }
