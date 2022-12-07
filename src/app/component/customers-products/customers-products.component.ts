a7e0f99437e397472e589375b70eb82abb14af20
JensIssa <71692396+JensIssa@users.noreply.github.com>
2022-12-07 14:08:46 +0100

    send it

--- src/app/component/customers-products/customers-products.component.ts
+++ src/app/component/customers-products/customers-products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {userService} from "../../../services/user.service";
 import {ProductService} from "../../../services/product.service";
+import jwtDecode from "jwt-decode";
+import {Token} from "../../../Interfaces/token";
 
 @Component({
   selector: 'app-customers-products',
@@ -18,7 +20,13 @@ export class CustomersProductsComponent implements OnInit {
   constructor(private http : ProductService) { }
 
   async ngOnInit() {
-    //this.productList = await this.http.getProductsFromUser();
+    let token = localStorage.getItem('token');
+    console.log(token)
+    if (!token)
+      return console.log("There was no matching token found")
+    let decodedToken = jwtDecode(token) as Token;
+
+    this.productList = await this.http.getProductsFromUser(decodedToken.id);
   }
 
   openEditProduct(item: any) {
