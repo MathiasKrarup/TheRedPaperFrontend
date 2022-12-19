f505990f1915e105a417e69c072862f4b6eb4230
JensIssa <71692396+JensIssa@users.noreply.github.com>
2022-12-19 03:24:11 +0100

    FIX PLEASE

--- src/app/component/adminview/adminview.component.ts
+++ src/app/component/adminview/adminview.component.ts
@@ -15,7 +15,7 @@ import {EditPasswordComponent} from "../edit-password/edit-password.component";
 export const customAxios = axios.create({
   baseURL: 'https://theredpaper.azurewebsites.net',
   headers: {
-    Authorization: `bearer ${localStorage.getItem('token')}`
+    Authorization: `Bearer ${localStorage.getItem('token')}`
   }
 })
 @Component({
